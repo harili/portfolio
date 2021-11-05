3bc7702f382a0517b6d87aa7656626699650a47c
harili <ilieschafyfy@gmail.com>
2021-11-06 00:37:06 +0100

    feature(not-resolved)[Trying to avoid refreshing page when changing view by passing by AJAX request]

---
+++ Models/Mail.cs
@@ -0,0 +1,152 @@
+﻿//using MailKit.Net.Smtp;
+//using MimeKit;
+//using System;
+//using System.IO;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+//namespace Hubos.Library.Tools
+//{
+//    public static class Mail
+//    {
+//        private static readonly string fromAddress = "webmaster@openspot.fr";
+//        private static readonly string fromAddressTitle = "HUBOS";
+//        private static readonly string smtpServer = "ssl0.ovh.net";
+//        private static readonly int smtpPortNumber = 587;
+//        private static readonly string credentialLogin = "webmaster@openspot.fr";
+//        private static readonly string credentialPassword = "rdelpech_2020";
+
+//        /// <summary>
+//        /// 
+//        /// </summary>
+//        /// <param name="targetList"></param>
+//        /// <param name="subject"></param>
+//        /// <param name="body"></param>
+//        public static async Task SendAsync(string[] targetList, string subject, string body, string[] attachmentList = null)
+//        {
+//            var message = new MimeMessage();
+//            var builder = new BodyBuilder();
+
+//            message.From.Add(new MailboxAddress(fromAddressTitle, fromAddress));
+//            foreach (string target in targetList)
+//            {
+//                message.To.Add(new MailboxAddress(target.Split("@").First(), target));
+//            }
+//            message.Subject = subject;
+//            builder.HtmlBody = body;
+
+//            if (attachmentList != null && attachmentList.Length > 0)
+//            {
+//                foreach (string attachment in attachmentList)
+//                {
+//                    builder.Attachments.Add(attachment);
+//                }
+//            }
+
+//            message.Body = builder.ToMessageBody();
+
+//            var client = new SmtpClient();
+//            client.AuthenticationMechanisms.Remove("XOAUTH2");
+//            await client.ConnectAsync(smtpServer, smtpPortNumber);
+//            await client.AuthenticateAsync(credentialLogin, credentialPassword);
+//            await client.SendAsync(message);
+//            await client.DisconnectAsync(true);
+//        }
+
+//        public static void Send(string[] targetList, string subject, string body)
+//        {
+//            var message = new MimeMessage();
+//            var builder = new BodyBuilder();
+
+//            message.From.Add(new MailboxAddress(fromAddressTitle, fromAddress));
+//            foreach (string target in targetList)
+//            {
+//                message.To.Add(new MailboxAddress(target.Split("@").First(), target));
+//            }
+//            message.Subject = subject;
+//            builder.HtmlBody = body;
+//            message.Body = builder.ToMessageBody();
+
+//            var client = new SmtpClient();
+//            client.AuthenticationMechanisms.Remove("XOAUTH2");
+//            client.Connect(smtpServer, smtpPortNumber);
+//            client.Authenticate(credentialLogin, credentialPassword);
+//            client.Send(message);
+//            client.Disconnect(true);
+//        }
+
+//        /// <summary>
+//        /// 
+//        /// </summary>
+//        /// <param name="targetList"></param>
+//        /// <param name="error"></param>
+//        /// <param name="extra"></param>
+//        public static void Send(string[] targetList, Exception error, string extra = null)
+//        {
+//            var message = new MimeMessage();
+//            var builder = new BodyBuilder();
+
+//            message.From.Add(new MailboxAddress(fromAddressTitle, fromAddress));
+//            foreach (string target in targetList)
+//            {
+//                message.To.Add(new MailboxAddress(target.Split("@").First(), target));
+//            }
+//            message.Subject = "ERROR " + DateTime.Now.ToString("yyyy-MM-dd");
+//            if (error.InnerException != null)
+//            {
+//                error = error.InnerException;
+//            }
+//            string body = "<h4>" + error.Message + "</h4><h5>TRACE CODE</h5>" + "<pre>" + error.StackTrace + "</pre>";
+//            if (!string.IsNullOrEmpty(extra))
+//            {
+//                body += "<h5>OTHER</h5><pre>" + extra + "</pre>";
+//            }
+//            builder.HtmlBody = body;
+//            message.Body = builder.ToMessageBody();
+
+//            var client = new SmtpClient();
+//            client.AuthenticationMechanisms.Remove("XOAUTH2");
+//            client.Connect(smtpServer, smtpPortNumber);
+//            client.Authenticate(credentialLogin, credentialPassword);
+//            client.Send(message);
+//            client.Disconnect(true);
+//        }
+
+//        /// <summary>
+//        /// 
+//        /// </summary>
+//        /// <param name="targetList"></param>
+//        /// <param name="error"></param>
+//        /// <param name="extra"></param>
+//        public static async Task SendAsync(string[] targetList, Exception error, string extra = null)
+//        {
+//            var message = new MimeMessage();
+//            var builder = new BodyBuilder();
+
+//            message.From.Add(new MailboxAddress(fromAddressTitle, fromAddress));
+//            foreach (string target in targetList)
+//            {
+//                message.To.Add(new MailboxAddress(target.Split("@").First(), target));
+//            }
+//            message.Subject = "ERROR " + DateTime.Now.ToString("yyyy-MM-dd");
+//            if (error.InnerException != null)
+//            {
+//                error = error.InnerException;
+//            }
+//            string body = "<h4>" + error.Message + "</h4><h5>TRACE CODE</h5>" + "<pre>" + error.StackTrace + "</pre>";
+//            if (!string.IsNullOrEmpty(extra))
+//            {
+//                body += "<h5>OTHER</h5><pre>" + extra + "</pre>";
+//            }
+//            builder.HtmlBody = body;
+//            message.Body = builder.ToMessageBody();
+
+//            var client = new SmtpClient();
+//            client.AuthenticationMechanisms.Remove("XOAUTH2");
+//            client.Connect(smtpServer, smtpPortNumber);
+//            client.Authenticate(credentialLogin, credentialPassword);
+//            await client.SendAsync(message);
+//            client.Disconnect(true);
+//        }
+//    }
+//}
--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,2 +1,60 @@
-﻿
+﻿$(document).ready(function () {
+    ui.controller.pageLoad();
+    $(window).on('hashchange', function (e) {
+        ui.controller.pageLoad(window.location.hash);
+    });
+});
 
+var ui = {
+    controller: {
+        page: {}
+    }
+};
+
+ui.controller.pageLoad = function (hash = null) {
+    if (hash === null) {
+        hash = window.location.hash;
+
+        if (hash.length < 2 || hash[0] !== "#") {
+            hash = $("ul.nav-pills li a").first().attr("href");
+        }
+    }
+    if (hash.length > 1 && hash[0] === "#") {
+        $("ul.nav-pills li a").removeClass("active");
+        $.get(hash.substring(1), function (data) {
+            console.log(data);
+            ui.controller.page = null;
+            $("#page-container").html(data);
+            ui.controller.pageInit();
+            ui.controller.pageInitAfter(hash);
+
+            var linkItem = $("ul.nav-pills li a[href='" + hash + "']");
+            linkItem.parent().addClass("active");
+            if (linkItem.hasClass("menu-link-sub")) {
+                var linkContainer = linkIt
+                linkContainer.parent().addClass("open"); em.parent().par
+                if ($("body").hasClass("mobile-view-activated"))
+                    $("body").removeClass("mobile-nav-on").addClass("blur"); ent();
+                linkContainer.show();
+            }
+        });
+    }
+};
+ui.controller.pageInit = function () {
+};
+ui.controller.pageInitAfter = function (hash) {
+};
+ui.controller.custom.pageInitAfter = function (hash) {
+};
+ui.controller.pageDestroy = function () {
+    ui.controller.pageDestroyContent();
+    ui.controller.page = null;
+};
+
+ui.controller.isValidHash = function (hash) {
+    return RegExp(/^#(\/[a-z0-9%-°.]+)+$/).test(hash);
+};
+
+ui.controller.urlRewrite = function (url) {
+    return url.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toLowerCase().replace(/ /g, '-').replace(/'/g, '').replace(/&/g, 'et');
+};
